3438defd4e28008ce84096b90dc248b94230a3a7
MarcoRMarques <161774713+MarcoRMarques@users.noreply.github.com>
2026-04-10 12:51:07 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -237,3 +237,19 @@ document.addEventListener("keydown", function (e) {
     }
   }
 });
+// ================================
+// 🔥 REGISTRO DO SERVICE WORKER
+// ================================
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then((reg) => {
+        console.log("✅ Service Worker registrado:", reg.scope);
+      })
+      .catch((err) => {
+        console.log("❌ Erro ao registrar Service Worker:", err);
+      });
+  });
+}
